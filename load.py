168e2d49100c1609239dd6d558617503c9867949
jbonfante <juan.bonfante@gmail.com>
2013-06-11 00:14:25 -0500

    Set All init layers to True
    In order to have world.load.run() execute all layers on first run, I've
    set all default params to true

--- load.py
+++ load.py
@@ -4,14 +4,13 @@ import os
 from django.contrib.gis.utils import LayerMapping
 from models import *
 
-__world_city = False
+__world_city = True
 __us_city = True
-__state = False
-__world = False
-__zip = False
-__urban = False
+__state = True
+__world = True
+__zip = True
+__urban = True
 __county = False
-__blockgroup = False
 
 
 
@@ -22,9 +21,9 @@ world_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/TM_WOR
 zip_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/zip_poly.shp'))
 urban_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/urban.shp'))
 county_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/dtl_cnty.shp'))
-blockgroup_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/blkgrp.shp'))
 
-def run(verbose=True, world_city = __world_city, us_city = __us_city, state = __state, world = __world, zipcode = __zip, urban = __urban, county = __county, blockgroup = __blockgroup):
+
+def run(verbose=True, world_city = __world_city, us_city = __us_city, state = __state, world = __world, zipcode = __zip, urban = __urban, county = __county):
     if world:
         lm = LayerMapping(WorldBorder, world_shp, worldborder_mapping,
                           transform=False, encoding='iso-8859-1')
@@ -40,11 +39,7 @@ def run(verbose=True, world_city = __world_city, us_city = __us_city, state = __
                           transform=False, encoding='iso-8859-1')
 
         lm.save(strict=False, verbose=verbose)
-    if blockgroup:
-        lm = LayerMapping(BlockGroupBorder, blockgroup_shp, blockgroupborder_mapping,
-                          transform=False, encoding='iso-8859-1')
 
-        lm.save(strict=False, verbose=verbose)
     if world_city:
         lm = LayerMapping(CityBorder, city_shp, cityborder_mapping,
                           transform=False, encoding='iso-8859-1')
