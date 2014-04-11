0552007afdfe9c5130d9bc1086c7158781c003e1
Juan Bonfante <juan.bonfante@gmail.com>
2014-04-11 15:20:47 -0500

    Added correct requirements file, added setup.py to allow pip install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-geo-world',
+    version='1.2',
+    packages=['django_geo_world'],
+    url='https://github.com/jbonfante/django-geo-world',
+    license='FreeBSD',
+    author='juanrbonfante',
+    author_email='juan@juanbonfante.com',
+    description='Django Geo Starter Pack inlcuding World cities, states, and us zip codes.'
+)
