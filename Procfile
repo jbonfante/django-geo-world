16a3342b4cb47fe7dc3985f12a6f0990aa0398e5
Juan Bonfante <juan.bonfante@gmail.com>
2014-07-03 18:15:27 -0500

    Added CookieCutter files
    Module [django-geo-world]:
      + project.css
      + .gitkeep
      + .gitkeep
      + project.js
      + project.scss
      + base.html
      + email.html
      + email_confirm.html
      + email_confirmed.html
      + login.html
      + logout.html
      + password_change.html
      + password_delete.html
      + password_delete_done.html
      + password_reset.html
      + password_reset_done.html
      + password_reset_from_key.html
      + password_reset_from_key_done.html
      + password_set.html
      + signup.html
      + signup_closed.html
      + verification_sent.html
      + verified_email_required.html
      + add.html
      + avatar_tag.html
      + base.html
      + change.html
      + confirm_delete.html
      + about.html
      + home.html
      + user_detail.html
      + user_form.html
      + user_list.html
      + 0001_initial.py
      + 0002_set_site_domain_and_name.py
      + __init__.py
      + __init__.py
      + settings.py
      + urls.py
      + wsgi.py
      + 404.html
      + 500.html
      + base.html
      + __init__.py
      + admin.py
      + forms.py
      + models.py
      + urls.py
      + views.py
      * __init__.py
      + manage.py
      + base.txt
      + local.txt
      + production.txt
      + test.txt
      + .editorconfig
      + .gitattributes
      * .gitignore
      + CONTRIBUTORS.txt
      + Gruntfile.js
      + LICENSE.rst
      + Procfile
      + README.rst
      + Vagrantfile
      + package.json
      * requirements.txt
      * setup.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath="$PWD/django_geo_world" config.wsgi:application
--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
-from distutils.core import setup
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import django_geo_world
+version = django_geo_world.__version__
 
 setup(
-    name='django-geo-world',
-    version='1.2',
-    packages=['django_geo_world'],
-    url='https://github.com/jbonfante/django-geo-world',
-    license='FreeBSD',
-    author='juanrbonfante',
-    author_email='juan@juanbonfante.com',
-    description='Django Geo Starter Pack inlcuding World cities, states, and us zip codes.'
-)
+    name='Django Geo World',
+    version=version,
+    author='Juan Bonfante',
+    author_email='juan.bonfante@gmail.com',
+    packages=[
+        'django_geo_world',
+        ],
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.6.1',
+        ],
+    zip_safe=False,
+    scripts=['django_geo_world/manage.py'],
+    description='Django Geo Starter Pack inlcuding World cities, states, and us zip codes.',
+    )
